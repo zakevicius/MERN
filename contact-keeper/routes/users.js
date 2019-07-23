adb766193c3586db92b9f6fa5e021da0b4f16548
zakevicius <m.zakevicius@gmail.com>
2019-07-23 23:10:33 +0300

    Server side Register and Login

--- contact-keeper/routes/users.js
+++ contact-keeper/routes/users.js
@@ -1,11 +1,74 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config')
+const { check, validationResult } = require('express-validator');
+
+const User = require('../models/User');
 
 // @route       POST api/users
 // @desc        Register a user
 // @access      Public
-router.post('/', (req, res) => {
-  res.send('Register a user');
-});
+router.post('/',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({
+      min: 6
+    })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    // Checking if user exists
+    try {
+      let user = await User.findOne({ email: email });
+
+      if (user) {
+        res.status(400).json({ msg: 'User already exists' });
+      }
+
+      // Creating new User
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      // Hashing password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      // Saving user to DB
+      await user.save();
+
+      // Creating JSON Web Token for user
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  });
 
 module.exports = router;
