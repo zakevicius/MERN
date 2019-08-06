d27734e3a7b3cbab35e843d6cb535f993fc28351
zakevicius <m.zakevicius@gmail.com>
2019-08-06 23:43:47 +0300

    IT-logger

---
+++ it-logger/src/App.js
@@ -0,0 +1,36 @@
+import React, { useEffect, Fragment } from 'react';
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
+import AppBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+import AddTechModal from './components/techs/AddTechModal';
+import TechListModal from './components/techs/TechListModal';
+
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+import './App.css';
+import AddBtn from './components/layout/AddBtn';
+
+const App = () => {
+  useEffect(() => {
+    // Initializes materialize JS
+    M.AutoInit();
+  });
+
+  return (
+    <Fragment>
+      <SearchBar />
+      <div className="container">
+        <AddBtn />
+        <AddLogModal />
+        <EditLogModal />
+        <AddTechModal />
+        <TechListModal />
+        <Logs />
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
